826ac01a245e03568a39702a111ed7ab565c6447
Bad0RANG3 <110911829+Bad0RANG3@users.noreply.github.com>
2026-08-17 13:13:33 +0800

    Delete qr-tool directory

--- qr-tool/start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting QR server on http://localhost:8787 ...
-start "QR Decoder Server" /min cmd /c "node server.js"
-timeout /t 2 /nobreak >nul
-start "" "http://localhost:8787"
